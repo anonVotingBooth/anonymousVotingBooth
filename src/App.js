8625e0ffacf59f7221b1f2cf544929efec978dad
Mehdi Pilehvarian <mehdi.pilehvarian@gmail.com>
2019-12-04 17:47:17 -0500

    firebase installed 🧔🏻 🎸

--- src/App.js
+++ src/App.js
@@ -1,26 +1,17 @@
 import React from 'react';
 import logo from './logo.svg';
 import './App.scss';
+import firebase from 'firebase';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+import React, { Component } from 'react';
+import './App.css';
+
+export default class App extends Component {
+    render() {
+        return (
+            <div className="App">
+                    
+            </div>
+        );
+    }
 }
-
-export default App;
---
+++ src/components/firebase.js
@@ -0,0 +1,15 @@
+import React from 'react';
+
+const firebaseConfig = {
+    apiKey: "AIzaSyARn2Fa86nETijeyn-afjgyoHlkusUXX4I",
+    authDomain: "anon-voting-booth.firebaseapp.com",
+    databaseURL: "https://anon-voting-booth.firebaseio.com",
+    projectId: "anon-voting-booth",
+    storageBucket: "anon-voting-booth.appspot.com",
+    messagingSenderId: "297294494416",
+    appId: "1:297294494416:web:e83fa5e90c4ea456967cd5"
+};
+// Initialize Firebase
+firebase.initializeApp(firebaseConfig);
+
+export default firebase;
