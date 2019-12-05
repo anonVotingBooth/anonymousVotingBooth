8745c6667a3e18b28f3f1fabd6d79373cb5c9779
Vincent Dang <vncntdang@gmail.com>
2019-12-05 12:57:28 -0500

    added list for polls :smiley_cat:

--- src/components/Polls.js
+++ src/components/Polls.js
@@ -1,12 +1,40 @@
 import React, { Component } from 'react';
+import firebase from 'firebase';
 
 class Polls extends Component {
+  constructor() {
+      super();
+      this.state = {
+        userInput: ''
+      };
+  }
+
+
+
   render() {
-
     return (
-      <div>
+        <ul className='pollsList'>
+          <li className='pollItem'>
+            <p>gif or jif?</p>
+            <form action="">
+              <button>gif</button>
+              <button>jif</button>
+              <button type=''>Submit</button>
+            </form>
+          </li>
+          <li className='pollItem'>
 
-      </div>
+          </li>
+          <li className='pollItem'>
+
+          </li>
+          <li className='pollItem'>
+
+          </li>
+          <li className='pollItem'>
+
+          </li>
+        </ul>
     );
   };
 };
