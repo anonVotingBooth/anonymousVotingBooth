ea8b3775675f4bc86785f481072c128023c4bcba
Jasmine StaRomana <jasmine@tiltedcanvas.com>
2019-12-09 16:37:12 -0500

    more animation work:sparkles:

--- src/components/AnimatedBackground.js
+++ src/components/AnimatedBackground.js
@@ -16,6 +16,12 @@ class AnimatedBackground extends Component {
                 <img id='imageEight' className='animatedImage' src={graphic2}></img>
                 <img id='imageNine' className='animatedImage' src={graphic1}></img>
                 <img id='imageTen' className='animatedImage' src={graphic2}></img>
+                {/* <img id='imageEleven' className='animatedImage' src={graphic1}></img>
+                <img id='imageTwelve' className='animatedImage' src={graphic2}></img>
+                <img id='imageThirteen' className='animatedImage' src={graphic1}></img>
+                <img id='imageFourteen' className='animatedImage' src={graphic2}></img>
+                <img id='imageFifteen' className='animatedImage' src={graphic1}></img>
+                <img id='imageSixteen' className='animatedImage' src={graphic2}></img> */}
             </div>
         )
     }
