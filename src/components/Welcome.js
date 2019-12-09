5b08842fbf643ea088da8d41904d12a17bcd4511
Mehdi Pilehvarian <mehdi.pilehvarian@gmail.com>
2019-12-09 16:22:56 -0500

    Added some Animation on the title 🎭 👨🏻‍💻

--- src/components/Welcome.js
+++ src/components/Welcome.js
@@ -56,9 +56,9 @@ class Welcome extends Component {
         
         return (
             <div>
-                <div className='welcomeSplash bounce-in-fwd'>
+                <div className='welcomeSplash'>
                     <div className='wrapper'>
-                        <img className='logo' src={logo}></img>
+                        <img className='logo bounce-in-fwd' src={logo}></img>
                         <div className='userLoginHome'>
                         {/* 
                         Commented this out as we don't need anymore, but left it just in case we change routes later. Delete before submitting project. -Jasmine
