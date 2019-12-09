48592de9deed1e5adb6fffd99f35913daa81af34
Sharon <sharon.yi@me.com>
2019-12-09 10:28:31 -0500

    i think im commiting the guest button

--- src/components/Welcome.js
+++ src/components/Welcome.js
@@ -4,15 +4,18 @@ import { BrowserRouter as Router, Route, Link } from 'react-router-dom';
 import logo from './../assets/logo.png';
 import firebase from 'firebase';
 import StyledFirebaseAuth from "react-firebaseui/StyledFirebaseAuth";
+import * as firebaseui from "firebaseui";
 
 const uiConfig = {
     signInFlow: "popup",
-    signInOptions: [{
-        provider: firebase.auth.GoogleAuthProvider.PROVIDER_ID,
-        customParameters: {
-            prompt: 'select_account'
-        }
-    }
+    signInOptions: [
+        {
+            provider: firebase.auth.GoogleAuthProvider.PROVIDER_ID,
+            customParameters: {
+                prompt: 'select_account'
+            },
+        },
+        firebaseui.auth.AnonymousAuthProvider.PROVIDER_ID,
     ],
     signInSuccessUrl: '/guest/dashboard',
     uiShown: function () {
@@ -30,18 +33,18 @@ const uiConfig = {
 class Welcome extends Component {
     render() {
         return (
-                <div className='welcomeSplash'>
-                    <div className='wrapper'>
-                        <img className='logo' src={logo}></img>
-                        <div className='userLoginHome'>
-                            <Link className='guestLoginButton' to='guest/dashboard'>guest login</Link>
-                            <Link to='/signup'>Sign Up</Link>
+            <div className='welcomeSplash'>
+                <div className='wrapper'>
+                    <img className='logo' src={logo}></img>
+                    <div className='userLoginHome'>
+                        <Link className='guestLoginButton' to='guest/dashboard'>guest login</Link>
+                        <Link to='/signup'>Sign Up</Link>
                         <Link to='/signinpage'>Sign In</Link>
                         <StyledFirebaseAuth uiConfig={uiConfig} firebaseAuth={firebase.auth()} />
 
-                        </div>
                     </div>
                 </div>
+            </div>
         );
     }
 }
