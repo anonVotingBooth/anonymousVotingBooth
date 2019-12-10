dacf38205ced9885d6d945eaec1e158aecde2555
Jasmine StaRomana <jasmine@tiltedcanvas.com>
2019-12-09 19:18:29 -0500

    changed animation timing:hourglass:

--- src/components/AnimatedBackground.js
+++ src/components/AnimatedBackground.js
@@ -15,20 +15,20 @@ class AnimatedBackground extends Component {
                     <img id='imageSix' className='animatedImage' src={graphic2}></img>
                 </div>
                 <div className='animatedBackground'>
-                    <img id='imageSeven' className='animatedImage' src={graphic1}></img>
-                    <img id='imageEight' className='animatedImage' src={graphic2}></img>
-                    <img id='imageNine' className='animatedImage' src={graphic1}></img>
-                    <img id='imageTen' className='animatedImage' src={graphic2}></img>
-                    <img id='imageEleven' className='animatedImage' src={graphic1}></img>
-                    <img id='imageTwelve' className='animatedImage' src={graphic2}></img>
+                    <img id='imageSeven' className='animatedImage2' src={graphic1}></img>
+                    <img id='imageEight' className='animatedImage2' src={graphic2}></img>
+                    <img id='imageNine' className='animatedImage2' src={graphic1}></img>
+                    <img id='imageTen' className='animatedImage2' src={graphic2}></img>
+                    <img id='imageEleven' className='animatedImage2' src={graphic1}></img>
+                    <img id='imageTwelve' className='animatedImage2' src={graphic2}></img>
                 </div>
                 <div className='animatedBackground'>
-                    <img id='imageThirteen' className='animatedImage' src={graphic1}></img>
-                    <img id='imageFourteen' className='animatedImage' src={graphic2}></img>
-                    <img id='imageFifteen' className='animatedImage' src={graphic1}></img>
-                    <img id='imageSixteen' className='animatedImage' src={graphic2}></img>
-                    <img id='imageSeventeen' className='animatedImage' src={graphic1}></img>
-                    <img id='imageEighteen' className='animatedImage' src={graphic2}></img>
+                    <img id='imageThirteen' className='animatedImage3' src={graphic1}></img>
+                    <img id='imageFourteen' className='animatedImage3' src={graphic2}></img>
+                    <img id='imageFifteen' className='animatedImage3' src={graphic1}></img>
+                    <img id='imageSixteen' className='animatedImage3' src={graphic2}></img>
+                    <img id='imageSeventeen' className='animatedImage3' src={graphic1}></img>
+                    <img id='imageEighteen' className='animatedImage3' src={graphic2}></img>
                 </div>
             </div>
         )
