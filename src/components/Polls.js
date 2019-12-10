0d0ef064db4181046ea0236f5dfd380845c54be6
Vincent Dang <vncntdang@gmail.com>
2019-12-10 16:16:11 -0500

    added unique user vote :smiley_cat:

--- src/components/Polls.js
+++ src/components/Polls.js
@@ -26,13 +26,12 @@ class Polls extends Component {
         const pollNumbers = {
           pNumber: key,
           pName: pollData[key],
-          voted: false
         }
         pollNum.push(pollNumbers);
       }
       this.setState({
         pollQuestions: pollContent,
-        currentPoll: pollNum
+        currentPoll: pollNum,
       });
     });
   }
@@ -42,52 +41,62 @@ class Polls extends Component {
     e.preventDefault();
     const {id} = e.currentTarget;
     const {currentPoll, pollQuestionRef} = this.state;
-    const vote = currentPoll[id].pName.votes1 + 1;
     const {userId} = this.props;
-    pollQuestionRef.child(`${currentPoll[id].pNumber}`).update({
-      votes1: vote,
-    });
-
-    pollQuestionRef.child(`${currentPoll[id].pNumber}`).child('usersVotedList').push(userId);
-    const newCurrentPoll = [...currentPoll]
-    newCurrentPoll[id].voted = true
-    this.setState({
-      answer1TotalVotes: vote,
-      currentPoll: newCurrentPoll
-    });
+    const usersVotedList = currentPoll[id].pName.usersVotedList;
+    const userList = [];
+    for (let user in usersVotedList) {
+      userList.push(usersVotedList[user])
+    }
+    if (!userList.includes(userId)) {
+      const vote = currentPoll[id].pName.votes1 + 1;
+      pollQuestionRef.child(`${currentPoll[id].pNumber}`).child('usersVotedList').push(userId);
+      pollQuestionRef.child(`${currentPoll[id].pNumber}`).update({
+        votes1: vote,
+      });
+      this.setState({
+        answer1TotalVotes: vote,
+      });
+    }
   }
+      // console.log(usersVotedList[users]);
 
   // Incrementing the option two every time a user votes for option two in any vote poll
   incrementAnswer2Count = (e) => {
     e.preventDefault();
     const {id} = e.currentTarget;
     const {currentPoll, pollQuestionRef} = this.state;
-    const vote = currentPoll[id].pName.votes2 + 1;
     const {userId} = this.props;
-    pollQuestionRef.child(`${currentPoll[id].pNumber}`).update({
-      votes2: vote
-    });
-
-    pollQuestionRef.child(`${currentPoll[id].pNumber}`).child('usersVotedList').push(userId);
-    const newCurrentPoll = [...currentPoll]
-    newCurrentPoll[id].voted = true
-    this.setState({
-      answer2TotalVotes: vote,
-      currentPoll: newCurrentPoll
-    });
-  }
+    const usersVotedList = currentPoll[id].pName.usersVotedList;
+    const userList = [];
+    for (let user in usersVotedList) {
+      userList.push(usersVotedList[user])
+    }
+    if (!userList.includes(userId)) {
+      currentPoll[id].voted = true;
+      const vote = currentPoll[id].pName.votes2 + 1;
+      pollQuestionRef.child(`${currentPoll[id].pNumber}`).child('usersVotedList').push(userId);
+      pollQuestionRef.child(`${currentPoll[id].pNumber}`).update({
+        votes2: vote,
+      });
+      this.setState({
+        answer2TotalVotes: vote,
+      });
+    }
+  } 
 
   render() {
     const {
       incrementAnswer1Count,
       incrementAnswer2Count,
       state: {
-        currentPoll,
-        pollQuestions
+        pollQuestions,
+        currentPoll
+      },
+      props: {
+        userId
       }
     } = this;
 
-    const {userId} = this.props;
     return (
         <div className="wrapper">
           <ul className='pollsList'>
@@ -99,78 +108,46 @@ class Polls extends Component {
                 const totalVotesB = value.votes2;
                 const pollQ = value.question;
                 const currentPollQ = index;
-                const voted = currentPoll[index].voted;
-                const usersVotedList = value.usersVotedList;
-                let item;
+                let voted = currentPoll[index].voted;
+                const usersVotedList = currentPoll[index].pName.usersVotedList;
+                const userList = [];
                 for (let user in usersVotedList) {
-                  if (usersVotedList[user] !== userId) {
-                    item = (
-                      <li key={index} className='pollItem'>
-                          <div className="pollQuestion">
-                            <p>{pollQ}</p>
-                          </div>
-                          <div className="voteButtons">
-                            <button 
-                              id={currentPollQ} 
-                              onClick={incrementAnswer1Count}  
-                              value='votes1'
-                              disabled={voted ? true : false}
-                            >
-                              {optionA}
-                              <p className={`${voted ? 'showVotes' : null}`}>
-                                ({totalVotesA})
-                              </p>
-                            </button>
-                            <button 
-                              id={currentPollQ}
-                              onClick={incrementAnswer2Count} 
-                              value='votes2'
-                              disabled={voted ? true : false}
-                            >
-                              {optionB}
-                              <p className={`${voted ? 'showVotes' : null}`}>
-                                ({totalVotesB})
-                              </p>
-                            </button>
-                          </div>
-                      </li>
-                    )
-                  }
-                  else {
-                    item = (
-                      <li key={index} className='pollItem'>
-                          <div className="pollQuestion">
-                            <p>{pollQ}</p>
-                          </div>
-                          <div className="voteButtons">
-                            <button 
-                              id={currentPollQ} 
-                              onClick={incrementAnswer1Count}  
-                              value='votes1'
-                              disabled={true}
-                            >
-                              {optionA}
-                              <p className='showVotes'>
-                                ({totalVotesA})
-                              </p>
-                            </button>
-                            <button 
-                              id={currentPollQ}
-                              onClick={incrementAnswer2Count} 
-                              value='votes2'
-                              disabled={true}
-                            >
-                              {optionB}
-                              <p className='showVotes'>
-                                ({totalVotesB})
-                              </p>
-                            </button>
-                          </div>
-                      </li>
-                    )
-                  }
+                  userList.push(usersVotedList[user])
                 }
-              return item;
+                if (userList.includes(userId)) {
+                  voted = true;
+                }
+                return (
+                  <li key={index} className='pollItem'>
+                      <div className="pollQuestion">
+                        <p>{pollQ}</p>
+                      </div>
+                      <div className="voteButtons">
+                        <button 
+                          id={currentPollQ} 
+                          onClick={incrementAnswer1Count}  
+                          value='votes1'
+                          disabled={voted ? true : false}
+                        >
+                          {optionA}
+                          <p className={voted ? 'showVotes' : null}>
+                            ({totalVotesA})
+                          </p>
+                        </button>
+                        <button 
+                          id={currentPollQ}
+                          onClick={incrementAnswer2Count} 
+                          value='votes2'
+                          disabled={voted ? true : false}
+                        >
+                          {optionB}
+                          <p className={voted ? 'showVotes' : null}>
+                            ({totalVotesB})
+                          </p>
+                        </button>
+                      </div>
+                  </li>
+                );
               })
             }
           </ul>
