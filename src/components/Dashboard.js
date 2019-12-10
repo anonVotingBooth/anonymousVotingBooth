df47270e55c3fdcb27cf6edffc28e5baeea09f42
Jasmine StaRomana <jasmine@tiltedcanvas.com>
2019-12-09 21:24:27 -0500

    toggle on create poll working:100::100::100:

--- src/components/Dashboard.js
+++ src/components/Dashboard.js
@@ -2,23 +2,26 @@ import React, { Component } from 'react';
 import Sidebar from './Sidebar';
 import CreateAPoll from './CreateAPoll';
 import Polls from './Polls';
-import ImageApi from './ImageApi';
+// import ImageApi from './ImageApi';
 // import ViewPolls from './ViewPolls';
 import '../App.scss';
-import firebase from 'firebase';
+// import firebase from 'firebase';
 import 'firebase/auth';
 
 class Dashboard extends Component {
     constructor(){
         super();
         this.state = {
-            currentView: null
+            currentView: null,
+            hidden: true
         }
     }
+    
     // this handle click shows the create a poll section above 'polls' when clicked. 
     handleClick = (e) => {
         this.setState({
-            currentView: e.target.id
+            currentView: e.target.id,
+            hidden: false
         })
     }
 
@@ -27,13 +30,14 @@ class Dashboard extends Component {
     // }
 
     render() {
+        
         return (
             <div className='flexDashboardParent'>
                 <Sidebar />
                 <div className='dashboard'>
-                    <button className='createPollButton' id='createPoll' onClick={this.handleClick}>+ create your own</button>
-                    {this.state.currentView === 'createPoll' && <CreateAPoll />}
-                    <Polls />
+                    {this.state.hidden && <div> <button className='createPollButton' id='createPoll' onClick={this.handleClick}>+ create your own</button> </div>}
+                    {this.state.currentView === 'createPoll' && <CreateAPoll />} 
+                    <Polls userId={this.props.userId}/>
                     {/* <ImageApi /> */}
                 </div>
             </div>
