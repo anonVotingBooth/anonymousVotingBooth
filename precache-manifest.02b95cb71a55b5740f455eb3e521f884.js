adbfb0d31bd7bf6448383b23de9c01311f60a8dd
Sharon <sharon.yi@me.com>
2019-12-10 20:46:23 -0500

    Updates

---
+++ precache-manifest.02b95cb71a55b5740f455eb3e521f884.js
@@ -0,0 +1,50 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3dc72055a7d39cbd57915cf789e659af",
+    "url": "/anonymousVotingBooth/index.html"
+  },
+  {
+    "revision": "a42bb52dff658a8ea7b9",
+    "url": "/anonymousVotingBooth/static/css/main.52f0dfae.chunk.css"
+  },
+  {
+    "revision": "f501ae02bc3b8fb992fe",
+    "url": "/anonymousVotingBooth/static/js/2.8d39d844.chunk.js"
+  },
+  {
+    "revision": "a42bb52dff658a8ea7b9",
+    "url": "/anonymousVotingBooth/static/js/main.9393151c.chunk.js"
+  },
+  {
+    "revision": "eec812dff5436254c481",
+    "url": "/anonymousVotingBooth/static/js/runtime-main.b726d54b.js"
+  },
+  {
+    "revision": "9fb0d5b733797305a1e05b65681294d8",
+    "url": "/anonymousVotingBooth/static/media/backgroundImage1.9fb0d5b7.svg"
+  },
+  {
+    "revision": "7897cea603d45bf5fc2844f1818d20e3",
+    "url": "/anonymousVotingBooth/static/media/backgroundImage2.7897cea6.svg"
+  },
+  {
+    "revision": "7e5d0b53426a8d53100a31deaf8fbe2f",
+    "url": "/anonymousVotingBooth/static/media/jasmineCarbone.7e5d0b53.jpg"
+  },
+  {
+    "revision": "f8abd45dc040d8d0ee1b0706b268b8ab",
+    "url": "/anonymousVotingBooth/static/media/logo.f8abd45d.svg"
+  },
+  {
+    "revision": "6b3f1d4ef5af700f80b26ee033878777",
+    "url": "/anonymousVotingBooth/static/media/mehdiPilehvarian.6b3f1d4e.jpg"
+  },
+  {
+    "revision": "c21f3cf519915c1105898459187cd817",
+    "url": "/anonymousVotingBooth/static/media/sharonYi.c21f3cf5.jpg"
+  },
+  {
+    "revision": "c68aa3ac9de8db0805968e9ae1512fc6",
+    "url": "/anonymousVotingBooth/static/media/vinceDang.c68aa3ac.jpg"
+  }
+]);
