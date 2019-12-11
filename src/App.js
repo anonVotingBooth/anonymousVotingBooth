33fee0b5302495d46f34f4a4a7c3bc2372b8401a
Jasmine StaRomana <jasmine@tiltedcanvas.com>
2019-12-10 20:26:42 -0500

    hire us we are great devs just look at our about:bangbang::bangbang:

--- src/App.js
+++ src/App.js
@@ -1,10 +1,6 @@
 import React, { Component } from 'react';
 import './App.scss';
 import { BrowserRouter as Router, Route, Redirect} from 'react-router-dom';
-<<<<<<< HEAD
-import About from './components/About';
-=======
->>>>>>> master
 import Welcome from './components/Welcome';
 import Dashboard from './components/Dashboard';
 import ViewPolls from './components/ViewPolls';
@@ -62,9 +58,6 @@ class App extends Component {
                         }
                         
                         return (<Dashboard signOut={handleSignOut} userId={userId}/>)
-                        }} />
-                    <Route path='/dashboard/create' component={CreateAPoll} />
-                    <Route path='/about' component={About} />
                     }} />
                     <Route path='/dashboard/viewpolls' render={() => (<ViewPolls userId={userId}/>)} />
                 </div>
